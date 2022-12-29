6d862698372eefb06146703363551ca67f645050
svalvastic <assogames2@gmail.com>
2022-12-30 00:30:38 +0100

    🎉 init(re initialization): add vite and slider

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div id="app">
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+// @import "/node_modules/bootstrap/scss/bootstrap.scss";
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
+body {
+  overflow-x: hidden;
+}
+#app {
+  overflow: hidden;
+  font-family: "Poppins", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
